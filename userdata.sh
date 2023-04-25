469c8e1bb7fe0ceaafe477ceae17810669155536
Raghu K <raghuk.vit@gmail.com>
2023-04-25 08:13:20 +0530

    Update tf-module-rabbitmq/

--- userdata.sh
+++ userdata.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+labauto ansible
 ansible-pull -i localhost, -U https://github.com/raghudevopsb71/roboshop-ansible roboshop.yml -e role_name=${component} -e env=${env} >/opt/ansible.log
